83e363f0f82cf96476c7f92de6237cfae004d399
Ricardo Carrillo <ricardoc@loymark.com>
2021-02-11 17:31:48 -0600

    Token Autorizacion para Dev

--- Web Resources/SharedLogic.js
+++ Web Resources/SharedLogic.js
@@ -13,6 +13,7 @@ Abox.SharedLogic = {
         Environment: "https://apidev.aboxplan.com",
         WebAPIEnvironment: "https://aboxcrmapidev.aboxplan.com",
         TokenForAboxServices: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNybV9hYm94YXBpIiwiYXBpIjp0cnVlLCJpYXQiOjE2MDMzMTIzODB9.Cu8FYQoVWDcof_qFZ5CIA6K2OYloOEn9F-b_XahLf9w",
+        TokenForWebAPI: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluIiwibmJmIjoxNjEzMDg0NjYwLCJleHAiOjE2MTMwODgyNjAsImlhdCI6MTYxMzA4NDY2MCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1NTg1OC8iLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjU1ODU4LyJ9.y-wxrFGdYg0my9bv3qQENuv5JyytL-4aXxHnECsrN0w"
     },
 
     Entities: {
