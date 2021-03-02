bb29f8e4adf2df9d7800f76ec062a7b0c5fe0f4e
Ricardo Carrillo <ricardoc@loymark.com>
2021-03-02 14:19:42 -0600

    Ajuste para Cache de Token por 50 minutos

--- CrmAboxApi/CrmAboxApi/Logic/Classes/Helper/OAuthMessageHandler.cs
+++ CrmAboxApi/CrmAboxApi/Logic/Classes/Helper/OAuthMessageHandler.cs
@@ -3,7 +3,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
+using System.Runtime.Caching;
 namespace Logic.CrmAboxApi.Classes.Helper
 {
     /// <summary>
@@ -15,10 +15,13 @@ namespace Logic.CrmAboxApi.Classes.Helper
         private AuthenticationParameters ap;
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
+        
         public OAuthMessageHandler(string serviceUrl, string clientId, string redirectUrl, string username, string password,
                 HttpMessageHandler innerHandler)
             : base(innerHandler)
         {
+            ObjectCache cache = MemoryCache.Default;
+            var token = cache["AuthToken"] as string;
             // Obtain the Azure Active Directory Authentication Library (ADAL) authentication context.
             //AuthenticationParameters ap = AuthenticationParameters.CreateFromResourceUrlAsync(
             //        new Uri(serviceUrl + "/api/data/")).Result;
@@ -27,24 +30,40 @@ namespace Logic.CrmAboxApi.Classes.Helper
 
             //Task<AuthenticationParameters> t = AuthenticationParameters.CreateFromResourceUrlAsync(
             //        new Uri(serviceUrl + "/api/data/"));
-            var task = Task.Run(async () => { await this.DiscoveryAuthority2(serviceUrl); });
-            task.Wait();
-            AuthenticationParameters ap = this.ap;
-
-            AuthenticationContext authContext = new AuthenticationContext(ap.Authority, false);
-            //Note that an Azure AD access token has finite lifetime, default expiration is 60 minutes.
-            AuthenticationResult authResult;
-            if (username != string.Empty && password != string.Empty)
+            if (token==null)
             {
-                UserCredential cred = new UserCredential(username, password);
-                authResult = authContext.AcquireToken(serviceUrl, clientId, cred);
+                var task = Task.Run(async () => { await this.DiscoveryAuthority2(serviceUrl); });
+                task.Wait();
+                AuthenticationParameters ap = this.ap;
+
+                AuthenticationContext authContext = new AuthenticationContext(ap.Authority, false);
+                //Note that an Azure AD access token has finite lifetime, default expiration is 60 minutes.
+                AuthenticationResult authResult;
+                if (username != string.Empty && password != string.Empty)
+                {
+                    UserCredential cred = new UserCredential(username, password);
+                    authResult = authContext.AcquireToken(serviceUrl, clientId, cred);
+                }
+                else
+                {
+                    authResult = authContext.AcquireToken(serviceUrl, clientId, new Uri(redirectUrl), PromptBehavior.Auto);
+                }
+
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(50);
+                cache.Set("AuthToken", authResult.AccessToken, policy);
+
+                authHeader = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+
             }
             else
             {
-                authResult = authContext.AcquireToken(serviceUrl, clientId, new Uri(redirectUrl), PromptBehavior.Auto);
+                authHeader = new AuthenticationHeaderValue("Bearer", token);
             }
+           
 
-            authHeader = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            
             //Logger.Debug($"AuthorizationToken: {authResult.AccessToken}");
         }
 
